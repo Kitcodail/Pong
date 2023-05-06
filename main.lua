650d4e3c85b24d58f82bd77fc6a52ed15969d57d
Nasir Hossain <kitcodail@gmail.com>
2023-05-06 08:33:05 -0500

    Added AABB collision detection

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 require('Entities/Player')
 
+
 function love.load()
     entities = {Player}
 
@@ -20,4 +21,13 @@ function love.draw()
     for i = 1, #entities do
         entities[i]:draw()
     end
+end
+
+
+function checkCollision(a, b) --Takes two arguments, the rectangles we want to check for collision.
+   if a.x + a.width > b.x and a.x < b.x + b.width and a.y + a.height > b.y and a.y < b.y + b.height then -- Is A, intersecting B
+      return true -- Returns the boolean value true, indicating that A and B are colliding.
+   else
+      return false -- Returns the boolean value false, indicating that A and B are not colliding.
+   end
 end
