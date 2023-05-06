fbe198f1cffc4bf0069d944a8c027e5229e71e27
Nasir Hossain <kitcodail@gmail.com>
2023-05-06 07:59:33 -0500

    Added entity system

--- main.lua
+++ main.lua
@@ -1,13 +1,23 @@
-function love.load()
+require('Entities/Player')
 
+function love.load()
+    entities = {Player}
+
+    for i = 1, #entities do
+        entities[i]:load()
+    end
 end
 
 
 function love.update(dt)
-
+    for i = 1, #entities do
+        entities[i]:update(dt)
+    end
 end
 
 
 function love.draw()
-
+    for i = 1, #entities do
+        entities[i]:draw()
+    end
 end
