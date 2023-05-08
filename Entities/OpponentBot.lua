efc8170ca457528aef2b40526c06aed3b9a86749
Nasir Hossain <kitcodail@gmail.com>
2023-05-08 16:28:57 -0500

    Updated the OpponentBot movement and fixed OpponentBot cross the bounds

--- Entities/OpponentBot.lua
+++ Entities/OpponentBot.lua
@@ -11,42 +11,48 @@ function OpponentBot:load()
     self.speed = 750
     self.yVel = 0
     self.timer = 0
+    self.move_time = 0.2
 end
 
 
 function OpponentBot:update(dt)
     self:move(dt)
-
+    self:checkBounds()
     self.timer = self. timer + dt
-    if self.timer > 0.4 then
-        self:moveTowardsY(Ball)
-        self.timer = 0
-    end
+
+    if self.timer > self.move_time then                                                                                                                                                         
+        self:moveTowardsY(Ball)                                                                                                                                                                 
+        self.timer = 0                                                                                                                                                                          
+        self.move_time = 0.2                                                                                                                                                                    
+    end                                                                                                                                                                                         
+    if checkCollision(self, Ball) then                                                                                                                                                          
+        self.move_time = 1                                                                                                                                                                      
+    end                                                                                                                                                                                         
+end                                                                                                                                                                                             
+                                                                                                                                                                                                
+                                                                                                                                                                                                
+function OpponentBot:draw()                                                                                                                                                                     
+    love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)                                                                                                                    
+end                                                                                                                                                                                             
+                                                                                                                                                                                                
+                                                                                                                                                                                                
+function OpponentBot:move(dt)                                                                                                                                                                   
+    self.y = self.y + self.yVel * dt                                                                                                                                                            
+    self.yVel = self.yVel * 0.975                                                                                                                                                               
+end                                                                                                                                                                                                
+                                                                                                                                                                                                
+function OpponentBot:checkBounds()                                                                                                                                                              
+    if self.y < 0 then                                                                                                                                                                          
+        self.y = 0                                                                                                                                                                              
+    end                                                                                                                                                                                         
+    if self.y > love.graphics.getHeight() - self.height then                                                                                                                                    
+        self.y = love.graphics.getHeight() - self.height                                                                                                                                        
+    end                                                                                                                                                                                         
+                                                                                                                                                                                                
 end
 
 
-function OpponentBot:draw()
-    love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
-end
-
-
-function OpponentBot:move(dt)
-    self.y = self.y + self.yVel * dt
-end
- 
-
-function OpponentBot:checkBounds()
-    if self.y < 0 then
-        self.y = 0
-    end
-    if self.y > love.graphics.getHeight() - self.height then
-        self.y = love.graphics.getHeight() - self.height
-    end
-    
-end
-
-function OpponentBot:moveTowardsY(target)
-    if target.y + target.height * 0.5 < self.y + self.height * 0.5 then
+function OpponentBot:moveTowardsY(target)    if target.y + target.height * 0.5 < self.y + self.height * 0.5 then
         self.yVel = -self.speed
     elseif target.y + target.height * 0.5 > self.y + self.height * 0.5 then
         self.yVel = self.speed
